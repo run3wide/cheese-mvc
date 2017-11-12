748a62e017ce47cd2e0c8144055a42789802205e
paul <paulvrobson@gmail.com>
2017-11-12 07:22:46 -0600

    Demo complete

--- src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
@@ -3,6 +3,8 @@ package com.launchcode.cheesemvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,17 +13,26 @@ import java.util.List;
 @RequestMapping("cheese")
 public class CheeseController {
 
+    static List<String> cheeses = new ArrayList<>();
+
     @RequestMapping(value = "")
     public String index(Model model) {
-
-        List<String> cheeses = new ArrayList();
-        cheeses.add("parmesan");
-        cheeses.add("gouda");
-        cheeses.add("cheddar");
 
         model.addAttribute("title", "My Cheeses");
         model.addAttribute("cheeses", cheeses);
 
         return "cheese/index";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName) {
+        cheeses.add(cheeseName);
+        return "redirect:";
     }
 }
