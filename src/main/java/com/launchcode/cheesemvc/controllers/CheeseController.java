1f99aa751b1c0facb9e960d33f08e941d3d5070f
paul <paulvrobson@gmail.com>
2017-11-12 20:39:01 -0600

    Add remove functionality

--- src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,5 +1,6 @@
 package com.launchcode.cheesemvc.controllers;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +8,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static List<String> cheeses = new ArrayList<>();
+    static Map<String, String> cheeses = new HashMap<>();
 
     @RequestMapping(value = "")
     public String index(Model model) {
@@ -31,8 +34,24 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName) {
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
+        if (!cheeses.containsKey(cheeseName)) {
+            cheeses.put(cheeseName, cheeseDescription);
+        }
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "/remove", method = RequestMethod.GET)
+    public String displayDeleteCheeseForm(Model model) {
+        model.addAttribute("title", "Remove Cheese");
+        model.addAttribute("cheeses", cheeses);
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "/remove", method = RequestMethod.POST)
+    public String deleteCheese(@RequestParam String cheese) {
+        cheeses.remove(cheese);
+
         return "redirect:";
     }
 }
